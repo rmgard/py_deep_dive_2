1bec44947c533944e8a93afd6899c8f0a25b2c5d
rmgard <ryan.m.gard@gmail.com>
2019-07-16 11:13:02 -0500

    finished with 2.9

--- 2.9_list_v_tuple.py
+++ 2.9_list_v_tuple.py
@@ -28,4 +28,44 @@ def fun1():
     pass
 
 
-dis(compile('(fn1, 10, 20)', 'string', 'eval'))
+dis(compile('(fun1, 10, 20)', 'string', 'eval'))
+
+'''
+A tuple (t1) making another tuple has the same memory address. 
+thus t2 = tuple(t1)
+id(t1) == id(t2) 
+because tuples are immutable, it doesn't make sense to have a different address
+'''
+
+t = tuple()
+prev = sys.getsizeof(t)
+for i in range(10):
+    c = tuple(range(i+1))
+    size_c = sys.getsizeof(c)
+    delta, prev = size_c - prev, size_c
+    print(f'{i+1} items: {size_c)')
+
+
+l = list()
+prev = sys.getsizeof(t)
+for i in range(10):
+    c = list(range(i+1))
+    size_c = sys.getsizeof(c)
+    delta, prev = size_c - prev, size_c
+    print(f'{i+1} items: {size_c)')
+
+c = list()
+prev = sys.getsizzeof(l)
+print(f'0 items: {prev}')
+for i in range(255):
+    c.append(i)
+    size_c = sys.getsizeof(c)
+    delta, prev = size_c - prev, size_c
+    print(f'{i+1} items: {size_c}, delta={delta}')
+
+t = tuple(range(100_000))
+l = list(t)
+
+timeit('t[99_999]', globals=globals(), number=10_000_000)
+timeit('l[99_999]', globals=globals(), number=10_000_000)
+
